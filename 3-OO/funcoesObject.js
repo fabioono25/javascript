50938d6894a7c7cc6f31d533221d44b4ef610a0d
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-07-10 14:24:46 -0300

    objects2

--- 3-OO/funcoesObject.js
+++ 3-OO/funcoesObject.js
@@ -27,3 +27,15 @@ Object.defineProperty(pessoa, 'dataNascimento', {
 pessoa.dataNascimento = '10/10/2019'
 console.log(pessoa.dataNascimento)
 
+//ECMAScript 2015 - ES6
+const dest = {a: 1}
+const o1 = {b: 2}
+const o2 = { c: 3, a: 4}
+
+const obj = Object.assign(dest, o1, o2) //atribuindo a um objeto de destino
+console.log(obj) //{ a: 4, b: 2, c: 3 }
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj) //{ a: 4, b: 2, c: 3 }
+
